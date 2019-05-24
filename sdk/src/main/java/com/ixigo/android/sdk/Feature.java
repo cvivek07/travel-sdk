4875f03738c26559bf709b5f71ec4e752bf011f8
Ananya Deva <ananya@ixigo.com>
2019-05-24 15:46:27 +0530

    Add new flight, hotel, bus and train features + deprecate old features

--- sdk/src/main/java/com/ixigo/android/sdk/Feature.java
+++ sdk/src/main/java/com/ixigo/android/sdk/Feature.java
@@ -8,8 +8,26 @@ import java.io.Serializable;
 
 public class Feature implements Serializable {
 
+    /**
+     * @deprecated To be removed in future. Use FLIGHT_HOME instead.
+     */
+    @Deprecated
     public static Feature FLIGHT_SEARCH = new Feature("/pwa/initialpage?page=FLIGHT_HOME", true, false);
+    public static Feature FLIGHT_HOME = new Feature("/pwa/initialpage?page=FLIGHT_HOME", true, false);
+    public static Feature HOTEL_HOME = new Feature("/pwa/initialpage?page=HOTEL_HOME", true, false);
+    public static Feature BUS_HOME = new Feature("/pwa/initialpage?page=BUS_HOME", true, false);
+    public static Feature TRAIN_HOME = new Feature("/pwa/initialpage?page=TRAIN_HOME", true, false);
+
+    /**
+     * @deprecated To be removed in future. Use FLIGHT_TRIPS instead.
+     */
+    @Deprecated
     public static Feature TRIPS = new Feature("/pwa/initialpage?page=FLIGHT_TRIPS", false, false);
+    public static Feature FLIGHT_TRIPS = new Feature("/pwa/initialpage?page=FLIGHT_TRIPS", false, false);
+    public static Feature HOTEL_TRIPS = new Feature("/pwa/initialpage?page=HOTEL_TRIPS", false, false);
+    public static Feature BUS_TRIPS = new Feature("/pwa/initialpage?page=BUS_TRIPS", false, false);
+    public static Feature TRAIN_TRIPS = new Feature("/pwa/initialpage?page=TRAIN_TRIPS", false, false);
+
     public static Feature WALLET = new Feature("/pwa/initialpage?page=IXIGO_MONEY", false, false);
 
     private String path;
