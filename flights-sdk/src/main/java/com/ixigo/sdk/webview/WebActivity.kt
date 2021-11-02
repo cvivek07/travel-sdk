7c1d07b8c8dbed7b62d80f66bef7e5f0a0bdce7d
Miguel Cohnen <miguel@ixigo.com>
2021-11-02 14:34:03 +0100

    Make statusBar text color dependent on status bar color

--- flights-sdk/src/main/java/com/ixigo/sdk/webview/WebActivity.kt
+++ flights-sdk/src/main/java/com/ixigo/sdk/webview/WebActivity.kt
@@ -1,14 +1,18 @@
 package com.ixigo.sdk.webview
 
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.ColorUtils
+import androidx.core.view.WindowInsetsControllerCompat
+import com.ixigo.sdk.flights.R
 import com.ixigo.sdk.flights.databinding.WebActivityBinding
 
 class WebActivity: AppCompatActivity(), WebViewFragmentDelegate {
 
     private lateinit var binding: WebActivityBinding
     private lateinit var webViewFragment: WebViewFragment
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +28,22 @@ class WebActivity: AppCompatActivity(), WebViewFragmentDelegate {
             .commit()
         setContentView(binding.root)
 
+        setStatusBarColor()
+
         supportFragmentManager.executePendingTransactions()
     }
 
     override fun quit() {
         finish()
+    }
+
+    private fun setStatusBarColor() {
+        val luminance = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ColorUtils.calculateLuminance(resources.getColor(R.color.ixigosdk_primary_color, theme))
+        } else {
+            ColorUtils.calculateLuminance(resources.getColor(R.color.ixigosdk_primary_color))
+        }
+        val isLightColor = luminance > 0.5
+        WindowInsetsControllerCompat(window, binding.root).isAppearanceLightStatusBars = isLightColor
     }
 }
--- flights-sdk/src/test/java/com/ixigo/sdk/webview/WebActivityTest.kt
+++ flights-sdk/src/test/java/com/ixigo/sdk/webview/WebActivityTest.kt
@@ -1,13 +1,20 @@
 package com.ixigo.sdk.webview
 
+import android.app.Activity
 import android.app.Application
 import android.content.Intent
+import android.os.Build
+import android.view.View
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ixigo.sdk.flights.R
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 class WebActivityTest {
@@ -17,6 +24,35 @@ class WebActivityTest {
 
     @Test
     fun `test calling quit finishes activity`() {
+        withWebActivity { activity ->
+            activity.quit()
+            val webViewFragment = activity.supportFragmentManager.fragments[0] as WebViewFragment
+            assertSame(activity, webViewFragment.delegate)
+            assertEquals(initialPageData, webViewFragment.requireArguments().getParcelable<InitialPageData>(WebViewFragment.INITIAL_PAGE_DATA_ARGS))
+            assertTrue(activity.isFinishing)
+        }
+    }
+
+    @Test
+    fun `test with light statusBar color`() {
+        assertStatusBar(true)
+    }
+
+    // `es` configures a dark status bar color
+    @Test @Config(qualifiers = "es")
+    fun `test with dark statusBar color`() {
+        assertStatusBar(false)
+    }
+
+    private fun assertStatusBar(expectedLight: Boolean) {
+        withWebActivity { activity ->
+            val rootView = activity.findViewById<View>(android.R.id.content).rootView
+            assertEquals(expectedLight, WindowInsetsControllerCompat(activity.window, rootView).isAppearanceLightStatusBars)
+            assertEquals(activity.resources.getColor(R.color.ixigosdk_primary_color), activity.window.statusBarColor)
+        }
+    }
+
+    private fun withWebActivity(activityAction: ActivityScenario.ActivityAction<WebActivity>) {
         val app = getApplicationContext<Application>()
         val intent = Intent(app, WebActivity::class.java).also {
             it.putExtra(
@@ -25,12 +61,6 @@ class WebActivityTest {
             )
         }
         val scenario = launchActivity<WebActivity>(intent)
-        scenario.onActivity { activity ->
-            activity.quit()
-            val webViewFragment = activity.supportFragmentManager.fragments[0] as WebViewFragment
-            assertSame(activity, webViewFragment.delegate)
-            assertEquals(initialPageData, webViewFragment.requireArguments().getParcelable<InitialPageData>(WebViewFragment.INITIAL_PAGE_DATA_ARGS))
-            assertTrue(activity.isFinishing)
-        }
+        scenario.onActivity(activityAction)
     }
 }
