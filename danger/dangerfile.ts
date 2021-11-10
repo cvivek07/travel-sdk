479c720a677b403107a0e469e2082e58a9a681d6
Miguel Cohnen <miguel@travenues.com>
2021-11-10 09:45:19 +0000

    Introduce Danger testing (testing name change)

---
+++ danger/dangerfile.ts
@@ -0,0 +1,40 @@
+import { scan } from "danger-plugin-lint-report";
+import { checkMergeRequestSize } from "@ixigo-packages/danger-plugin-merge-request-size";
+
+const mergeRequestSizeOptions = {
+  rules: [
+    {
+      minSize: 0,
+      maxSize: 50,
+      name: "x-small",
+      type: "message",
+    },
+    {
+      minSize: 50,
+      maxSize: 100,
+      name: "small",
+      type: "message",
+    },
+    {
+      minSize: 100,
+      maxSize: 150,
+      name: "medium",
+      type: "warn",
+    },
+    {
+      minSize: 150,
+      maxSize: Number.MAX_SAFE_INTEGER,
+      name: "large",
+      type: "warn",
+    },
+  ],
+};
+
+(async () => {
+  await checkMergeRequestSize(mergeRequestSizeOptions);
+  await scan({
+    fileMask: "**/reports/lint-results*.xml",
+    reportSeverity: true,
+    requireLineModification: true,
+  });
+})();
