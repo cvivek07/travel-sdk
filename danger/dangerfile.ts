b179be0559cc6a4fbdbc2ee30dcc5f438a75ac50
Miguel Cohnen <miguel@ixigo.com>
2021-12-13 14:10:58 -0300

    ci: use ixigo common danger library

--- danger/dangerfile.ts
+++ danger/dangerfile.ts
@@ -1,47 +1,17 @@
 import { scan } from "danger-plugin-lint-report";
-import { checkMergeRequestSize } from "@ixigo-packages/danger-plugin-merge-request-size";
-import commitlint from "danger-plugin-conventional-commitlint";
-import configConventional from "@commitlint/config-conventional";
+import {
+  checkMergeRequestSize,
+  commitlint,
+} from "@ixigo-packages/ixigo-danger-common";
 import { diffCoverage } from "./diffCoverage";
 
-const mergeRequestSizeOptions = {
-  rules: [
-    {
-      minSize: 0,
-      maxSize: 50,
-      name: "x-small",
-      type: "message",
-    },
-    {
-      minSize: 50,
-      maxSize: 100,
-      name: "small",
-      type: "message",
-    },
-    {
-      minSize: 100,
-      maxSize: 150,
-      name: "medium",
-      type: "warn",
-    },
-    {
-      minSize: 150,
-      maxSize: Number.MAX_SAFE_INTEGER,
-      name: "large",
-      type: "warn",
-    },
-  ],
-};
-
 (async () => {
-  await checkMergeRequestSize(mergeRequestSizeOptions);
+  await checkMergeRequestSize();
   await scan({
     fileMask: "**/reports/lint-results*.xml",
     reportSeverity: true,
     requireLineModification: true,
   });
-  await commitlint(configConventional.rules, {
-    severity: "warn",
-  });
-  await diffCoverage()
+  await commitlint();
+  await diffCoverage();
 })();
