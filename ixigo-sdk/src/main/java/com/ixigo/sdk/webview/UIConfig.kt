fe626c519db52a1c0fa48794e6e2d6d094f100d6
Miguel Cohnen <miguel@ixigo.com>
2022-06-01 15:03:07 +0200

    fix: add Keep annotation to configUI classes

--- ixigo-sdk/src/main/java/com/ixigo/sdk/webview/UIConfig.kt
+++ ixigo-sdk/src/main/java/com/ixigo/sdk/webview/UIConfig.kt
@@ -4,16 +4,17 @@ import androidx.annotation.Keep
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
+@Keep
 sealed class BackNavigationMode {
 
   override fun equals(other: Any?) = other?.javaClass == javaClass
   override fun hashCode(): Int = javaClass.hashCode()
 
-  @JsonClass(generateAdapter = true) class Enabled : BackNavigationMode()
+  @JsonClass(generateAdapter = true) @Keep class Enabled : BackNavigationMode()
 
-  @JsonClass(generateAdapter = true) class Handler : BackNavigationMode()
+  @JsonClass(generateAdapter = true) @Keep class Handler : BackNavigationMode()
 
-  @JsonClass(generateAdapter = true) class Disabled : BackNavigationMode()
+  @JsonClass(generateAdapter = true) @Keep class Disabled : BackNavigationMode()
 }
 
 @Keep data class UIConfig(val backNavigationMode: BackNavigationMode)
