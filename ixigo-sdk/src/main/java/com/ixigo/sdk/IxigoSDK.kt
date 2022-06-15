3dfddfb48e32289f489de6d4c2790dafb9bf6185
vivek.choudhary@travenues.com <>
2022-06-15 13:43:59 +0530

    Updated visibility modifier

--- ixigo-sdk/src/main/java/com/ixigo/sdk/IxigoSDK.kt
+++ ixigo-sdk/src/main/java/com/ixigo/sdk/IxigoSDK.kt
@@ -148,7 +148,7 @@ internal constructor(
    * @param context
    * @param url url to open the activity
    */
-  internal fun launchWebActivity(context: Context, url: String, config: FunnelConfig? = null) {
+  fun launchWebActivity(context: Context, url: String, config: FunnelConfig? = null) {
     val intent = Intent(context, WebActivity::class.java)
     intent.putExtra(WebViewFragment.INITIAL_PAGE_DATA_ARGS, InitialPageData(url, getHeaders(url)))
     config?.let { intent.putExtra(WebViewFragment.CONFIG, it) }
