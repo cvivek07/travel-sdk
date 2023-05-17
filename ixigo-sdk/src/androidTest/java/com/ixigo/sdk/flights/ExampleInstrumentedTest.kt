f5774f65f64ca6715bcd0bde557e93ffdf06f249
vishal.arora <vishal.arora@travenues.com>
2023-05-17 18:01:41 +0530

    fix test

--- ixigo-sdk/src/androidTest/java/com/ixigo/sdk/flights/ExampleInstrumentedTest.kt
+++ ixigo-sdk/src/androidTest/java/com/ixigo/sdk/flights/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
   fun useAppContext() {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.ixigo.sdk.flights.test", appContext.packageName)
+    assertEquals("com.ixigo.sdk.test", appContext.packageName)
   }
 }
