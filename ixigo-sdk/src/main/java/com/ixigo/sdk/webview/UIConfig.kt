bf61384ee57c1c7fd056aacc71b6ee476e269d66
vishal.arora <vishal.arora@travenues.com>
2023-08-09 23:11:04 +0530

    fix: back navigation mode

--- ixigo-sdk/src/main/java/com/ixigo/sdk/webview/UIConfig.kt
+++ ixigo-sdk/src/main/java/com/ixigo/sdk/webview/UIConfig.kt
@@ -3,18 +3,18 @@ package com.ixigo.sdk.webview
 import androidx.annotation.Keep
 import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
+@JsonClass(generateAdapter = false)
 @Keep
 sealed class BackNavigationMode {
 
   override fun equals(other: Any?) = other?.javaClass == javaClass
   override fun hashCode(): Int = javaClass.hashCode()
 
-  @JsonClass(generateAdapter = true) @Keep class Enabled : BackNavigationMode()
+  @JsonClass(generateAdapter = false) @Keep class Enabled : BackNavigationMode()
 
-  @JsonClass(generateAdapter = true) @Keep class Handler : BackNavigationMode()
+  @JsonClass(generateAdapter = false) @Keep class Handler : BackNavigationMode()
 
-  @JsonClass(generateAdapter = true) @Keep class Disabled : BackNavigationMode()
+  @JsonClass(generateAdapter = false) @Keep class Disabled : BackNavigationMode()
 }
 
 @Keep data class UIConfig(val backNavigationMode: BackNavigationMode)
